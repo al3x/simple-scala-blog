0ebc3db26f6d46f9a3b17b6fea2de6ffbee3945a
Alex Payne <al3x@al3x.net>
2008-11-22 23:17:15 -0800

    generates workable output.

--- bin/blog.scala
+++ bin/blog.scala
@@ -1,19 +1,28 @@
-import java.io.{BufferedReader, ByteArrayOutputStream, File, FileInputStream, FileReader, FileWriter}
+import java.io.{File, FileReader, FileWriter}
+import java.lang.Runtime
 import net.sf.jtextile.JTextile
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
+import scala.xml.XML
 
-val postDir = (new File("../posts"))
-var posts = new ListBuffer[File]()
+val postDir = "../posts"
+val postTemplate = new File("../templates/post.html")
+var postFiles = new ListBuffer[File]()
+val wwwDir = "../www"
 
 def findPosts(dir: File) {
   for (file <- dir.listFiles) {
     if (file.isFile && file.getName.endsWith(".textile")) {
-      posts += file
+      postFiles += file
     } else if (file.isDirectory) {
       findPosts(file)
     }
   }
+}
+
+def readTitle(file: File): String = {
+  val src = Source.fromFile(file)
+  src.getLine(1).split("h1. ")(1)
 }
 
 def readFile(file: File): String = {
@@ -21,27 +30,51 @@ def readFile(file: File): String = {
   src.getLines.mkString
 }
 
-def readFileToString(f: File) = {
-  val bos = new ByteArrayOutputStream
-  val ba = new Array[Byte](2048)
-  val is = new FileInputStream(f)
+def writeFile(file: File, contents: String) = {
+  file.createNewFile
+  val writer = new FileWriter(file)
+  writer.write(contents)
+  writer.flush
+  writer.close
+}
 
-  def read {
-    is.read(ba) match {
-      case n if n < 0 =>
-        case 0 => read
-      case n => bos.write(ba, 0, n); read
-    }
+def templatizePost(post: File): String = {
+  val title = readTitle(post)
+  val body = readFile(post)
+  val textiled = JTextile.textile(body).trim
+  val temp = readFile(postTemplate)
+  var out = temp.replace("XTITLE", title)
+  out.replace("XBODY", textiled)
+}
+
+def filePost(post: File) = {
+  val body = templatizePost(post)
+
+  val parts = post.getPath.split("posts/")(1).split("/")
+  val year = parts(0)
+  val month = parts(1)
+  val day = parts(2)
+  val filename = parts(3).split(".textile")(0)
+
+  val outDir = new File(Array(wwwDir, year, month, day).mkString("/"))
+  if (!outDir.exists && outDir.mkdirs) {
+    println("created directory " + outDir.getPath)
   }
 
-  read
-  bos.toString("UTF-8")
+  val outFile = new File(Array(wwwDir, year, month, day, filename).mkString("/") + ".html")
+  //if (!outFile.exists) {
+    outFile.delete
+    writeFile(outFile, body)
+    println("created file " + outFile.getPath)
+  //}
+
+  val symlinkFile = new File(Array(wwwDir, year, month, filename).mkString("/") + ".html")
+  if (!symlinkFile.exists) {
+    val symCmd = "ln -s " + outFile.getPath + " " + symlinkFile.getPath
+    Runtime.getRuntime.exec(symCmd)
+    println("symlinked file " + symlinkFile.getPath)
+  }
 }
 
-findPosts(postDir)
-
-for (post <- posts) {
-  val body = readFileToString(post)
-  val textiled = JTextile.textile(body)
-  println(textiled)
-}
+findPosts(new File(postDir))
+postFiles.map(filePost)
